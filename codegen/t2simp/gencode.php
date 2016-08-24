b14d05f402ba0d232b2483944d67f812973cf2b3
斟酌 鵬兄 <tgckpg@gmail.com>
2016-08-24 15:37:53 +0800

    t2s has phase 2

--- codegen/t2simp/gencode.php
+++ codegen/t2simp/gencode.php
@@ -2,5 +2,6 @@
 
 include __DIR__ . "/../mkcstable.php";
 
-$wconv_t = get_function_group(__DIR__ . "/word_t2s.txt", "w_");
+$wconv_t = get_function_group(__DIR__ . "/phrase_t2s.txt", "p_");
+$wconv_t .= get_function_group(__DIR__ . "/word_t2s.txt", "w_");
 file_put_contents(__DIR__ . "/../../native/t2simp/wconv_gtable.cpp", $wconv_t);
--- native/t2simp/wconv_gtable.h
+++ native/t2simp/wconv_gtable.h
@@ -1,6 +1,1 @@
-void match_w_byte6(unsigned char const* str, int start, char const* &out, int &step_size);
-void match_w_byte5(unsigned char const* str, int start, char const* &out, int &step_size);
-void match_w_byte4(unsigned char const* str, int start, char const* &out, int &step_size);
-void match_w_byte3(unsigned char const* str, int start, char const* &out, int &step_size);
-void match_w_byte2(unsigned char const* str, int start, char const* &out, int &step_size);
-void match_w_ascii(unsigned char const* str, int start, char const* &out, int &step_size);
+../s2trad/wconv_gtable.h
